d66d3e0de344e65472377dda2c3ba995ee21f65f
mwells <mwells@haystax.com>
2020-03-18 20:33:41 -0400

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Myron Wells on 3/18/20.
+//  Copyright © 2020 Myron Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
